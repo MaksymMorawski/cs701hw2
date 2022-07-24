ffbd48d825014176f75b45fef8535bf5b75dacea
Maksym Morawski <max@Maksyms-MBP.attlocal.net>
2022-07-23 19:51:45 -0700

    Fixed some whitespace

--- part1.py
+++ part1.py
@@ -1,2 +1,4 @@
 '''
-Suppose a string variable season holds the current season, a float variable temperature holds the current temperature, and a boolean variable air_conditioner indicates if the air conditioner is on. Write a Python program that, if the season is "summer", then if the temperature is above 72 it should set air_conditioner to True and otherwise set it to False.'''
+Suppose a string variable season holds the current season, a float variable temperature holds the current temperature, and a boolean variable air_conditioner indicates if the air conditioner is on. 
+Write a Python program that, if the season is "summer", then if the temperature is above 72 it should set air_conditioner to True and otherwise set it to False.
+'''
